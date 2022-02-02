5bfa222be323b05c77550e54c4e3831444297da3
krlkonstantine <kostea.023@gmail.com>
2022-02-02 08:04:06 +0300

    Updates

---
+++ precache-manifest.2fdb0e407e486a90ca6db4c918ab6099.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403657d93fdb31c4989160a3715be077",
+    "url": "/test_app_deploy/index.html"
+  },
+  {
+    "revision": "2960e50d1437fa4c0ca0",
+    "url": "/test_app_deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3b80c6aff465163f2b85",
+    "url": "/test_app_deploy/static/js/2.340a7f56.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/test_app_deploy/static/js/2.340a7f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44cb569186e3209754d",
+    "url": "/test_app_deploy/static/js/3.815f61f1.chunk.js"
+  },
+  {
+    "revision": "2960e50d1437fa4c0ca0",
+    "url": "/test_app_deploy/static/js/main.36ad45cb.chunk.js"
+  },
+  {
+    "revision": "18a6d3d6ea6eb14d1446",
+    "url": "/test_app_deploy/static/js/runtime-main.07ee2ac2.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/test_app_deploy/static/media/logo.06e73328.svg"
+  }
+]);
